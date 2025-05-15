f49a5be1c32f2d890daaa893005dd7b196956838
aIcantara <152627480+aIcantara@users.noreply.github.com>
2025-05-15 17:13:35 +0300

    AsiPlugin

---
+++ src/Render.h
@@ -0,0 +1,31 @@
+#ifndef RENDER_H
+#define RENDER_H
+
+#include <kthook/kthook.hpp>
+#include <imgui.h>
+#include <d3d9.h>
+
+class CRender {
+public:
+  CRender();
+  ~CRender();
+
+  bool changeWindowState();
+private:
+  bool window = false;
+
+  void pauseScreen(bool state);
+  std::uintptr_t findDevice(std::uint32_t length);
+  void* getFunctionAddress(int VTableIndex);
+
+  kthook::kthook_signal<HRESULT(__stdcall*)(IDirect3DDevice9*, const RECT*, const RECT*, HWND, const RGNDATA*)> hookPresent;
+  std::optional<HRESULT> onPresent(const decltype(hookPresent)& hook, IDirect3DDevice9* pDevice, const RECT*, const RECT*, HWND, const RGNDATA*);
+
+  kthook::kthook_signal<HRESULT(__stdcall*)(IDirect3DDevice9*, D3DPRESENT_PARAMETERS*)> hookReset;
+  std::optional<HRESULT> onLost(const decltype(hookReset)& hook, IDirect3DDevice9* pDevice, D3DPRESENT_PARAMETERS* parameters);
+  void onReset(const decltype(hookReset)& hook, HRESULT& returnValue, IDirect3DDevice9* pDevice, D3DPRESENT_PARAMETERS* parameters);
+};
+
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+#endif // RENDER_H
---
+++ src/dllmain.cpp
@@ -0,0 +1,15 @@
+#include "Plugin.h"
+#include <memory>
+
+std::unique_ptr<CPlugin> plugin;
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved) {
+  if (dwReason == DLL_PROCESS_ATTACH) {
+    DisableThreadLibraryCalls(hModule);
+    plugin = std::make_unique<CPlugin>(hModule);
+  }
+  else if (dwReason == DLL_PROCESS_DETACH) {
+    plugin.reset();
+  }
+  return TRUE;
+}
